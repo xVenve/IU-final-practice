a5d84a7d7304d2cd394d434c5d2e14e536073795
NuriaGarciaRozas <yomellamonuria@gmail.com>
2020-12-01 19:41:12 +0100

    1.2 Es problema de LiveServer?

--- script/script.js
+++ script/script.js
@@ -1,22 +1,28 @@
 var cuentaActiva = null;
 
+
+$(document).ready(function(){
+    $("#calendario").simpleCalendar({
+        months: ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','November','Diciembre'],
+        days: ['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado'],
+        displayYear: true,              // Display year in header
+        fixedStartDay: true,            // Week begin always by monday or by day set by number 0 = sunday, 7 = saturday, false = month always begin by first day of the month
+        displayEvent: true,             // Display existing event
+        disableEventDetails: false, // disable showing event details
+        disableEmptyDetails: true, // disable showing empty date details
+        events: [{
+            startDate: new Date("2020-12-05 21:35"),
+            endDate: new Date("2020-12-05"),
+            summary: 'CUMPLEAÑOS DE LA FANTÁSTICA Y MARAVILLOSA NURIA GARCÍA ROZAS'
+        },{
+            startDate: new Date("2020-12-06"),
+            endDate: new Date("2020-12-06"),
+            summary: 'DIA DE LA CONSTITUCION DE ESPAÑITA'
+        }
+        ]});
+});
+
 function start() {
-    document.getElementById("iniciosesion").style.display = "block";
-}
-
-function guardarCookie() {
-    var NIA = document.getElementById('formNIA').value;
-    var contraseña = document.getElementById('formcontraseña').value;
-    var nombre = document.getElementById('formnombre').value;
-    var apellidos = document.getElementById('formapellidos').value;
-    var email = document.getElementById('formemail').value;
-    var roles = document.getElementById('formrol').selectedOptions[0].value;
-    var sep = "#";
-    var cookieCompleta = email + "=" + contraseña + sep + nombre + sep + apellidos + sep + roles + sep + NIA;
-    document.cookie = cookieCompleta;
-
-    document.getElementById('vertical-line').style.display = "none";
-    document.getElementById('registro').style.display = "none";
     document.getElementById('iniciosesion').style.paddingTop = "2%";
     document.getElementById('iniciosesion').style.width = "100%";
     document.getElementById('formularios').style.marginLeft = "38%";
@@ -24,8 +30,26 @@ function guardarCookie() {
     document.getElementById('formularios').style.width = "25%";
 }
 
+function guardarCookie() {
+    var contraseña = document.getElementById('formcontraseña').value;
+    var nombre = document.getElementById('formnombre').value;
+    var apellidos = document.getElementById('formapellidos').value;
+    var email = document.getElementById('formemail').value;
+    var roles = document.getElementById('formrol').selectedOptions[0].value;
+    var NIA = document.getElementById('formNIA').value;
+    var sep = "#";
+    var cookieCompleta = email + "=" + contraseña + sep + nombre + sep + apellidos + sep + roles + sep + NIA;
+    document.cookie = cookieCompleta;
+    
+    $("#vertical-line").fadeOut("slow");
+    $("#registro").fadeOut("slow");
+    start();
+}
+
 function desplegarRegistro() {
-    document.getElementById('vertical-line').style.display = "inline-block";
+    $("#vertical-line").fadeIn("slow");
+    $("#registro").fadeIn("slow");
+    document.getElementById('vertical-line').style.display = "absolute";
     document.getElementById('registro').style.display = "block";
     document.getElementById('iniciosesion').style.paddingTop = "10%";
     document.getElementById('formularios').style.marginLeft = "20%";
@@ -34,6 +58,20 @@ function desplegarRegistro() {
     document.getElementById('iniciosesion').style.width = "40%";
 }
 
+
+function desplegar_contenido(x){
+    var contenido = document.getElementById(x);
+    $(contenido).slideToggle();
+}
+
+function desplegar_asignatura(x){
+    var asignatura = document.getElementById(x);
+    asignatura.style.display ="block";
+    var home = document.getElementById("div_content");
+    home.style.display ="none";
+    $(".barralateralderecha").show();
+
+}
 function obtenerCookie(email) {
     var emailigual = email + "=";
     var restoCookie = document.cookie.split(emailigual)[1];
@@ -50,22 +88,63 @@ function inicioSesion() {
     var contraseñaInicio = document.getElementById('iniciocontraseña').value;
     var elementosCookie = obtenerCookie(emailInicio);
     var contraseñaCookie = elementosCookie[0];
-    var rolCookie = elementosCookie[1];
+    var rolCookie = elementosCookie[3];
+    var usernameCookie = elementosCookie[1] + " " + elementosCookie[2];
     if (contraseñaInicio === contraseñaCookie) {
         cuentaActiva = emailInicio;
-        alert(cuentaActiva);
-        /*window.addEventListener("resize", responsive);
-        document.getElementById('inicioformulario').style.display="none";
-        document.getElementById('menucabecera').style.display="block";
-        document.getElementById('perfil').style.display="inline";*/
+        //window.addEventListener("resize", responsive);
+        document.getElementById('formularios').style.display="none";
+        document.getElementById('cabecera2').style.display="block";
+        document.getElementById('principal').style.display="flex";
+        document.getElementById('perfil').style.display="inline";
+        document.getElementById('abajo').style.display="block";
+        document.getElementById('lateral').style.display="block";
         if (rolCookie === "Estudiante") {
             estudiante();
-        } else {
-            adminprofe();
+        } else if(rolCookie === "Profesor"){
+            profesor();
+        }else{
+            administrador();
         }
-        /*document.getElementById('nombreusuario').innerHTML=usernameCookie;
-        responsive();
+        document.getElementById('nombreusuario').innerHTML=usernameCookie;
+        /*responsive();
         Inicio();*/
     }
 }
 
+function estudiante(){
+    $(".admin").hide();
+    $(".profesor").hide();
+    $(".estudiante").show();
+}
+
+//MOSTRAR ROL PROFESOR
+function profesor(){
+    $(".admin").hide();
+    $(".estudiante").hide();
+    $(".profesor").show();
+}
+
+function administrador(){
+    $(".profesor").hide();
+    $(".estudiante").hide();
+    $(".admin").show();
+}
+
+function cerrarSesion(){
+    var confirmacion=confirm("¿Quieres cerrar sesión?");
+    if(confirmacion){
+        start();
+        $("#principal").hide();
+        $("#formularios").show();
+        $("#perfil").hide();
+        $("#cabecera2").hide();
+        $("#abajo").hide();
+}
+}
+
+function Inicio(){
+    $("#div_content").show();
+    $("#IU").hide();
+
+}
