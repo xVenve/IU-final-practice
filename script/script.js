5344002ad72311716d16829c1125de84d71e874e
Jorge Rodríguez <jrf1616@gmail.com>
2020-12-03 01:46:37 +0100

    1.4 Ajustadas la tabulaciones para mas claridad

--- script/script.js
+++ script/script.js
@@ -1,25 +1,25 @@
 var cuentaActiva = null;
 
 
-$(document).ready(function(){
-    $("#calendario").simpleCalendar({
-        months: ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','November','Diciembre'],
-        days: ['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado'],
-        displayYear: true,              // Display year in header
-        fixedStartDay: true,            // Week begin always by monday or by day set by number 0 = sunday, 7 = saturday, false = month always begin by first day of the month
-        displayEvent: true,             // Display existing event
-        disableEventDetails: false,     // disable showing event details
-        disableEmptyDetails: true,      // disable showing empty date details
-        events: [{
-            startDate: new Date("2020-12-05 21:35"),
-            endDate: new Date("2020-12-05"),
-            summary: 'CUMPLEAÑOS DE LA FANTÁSTICA Y MARAVILLOSA NURIA GARCÍA ROZAS'
-        },{
-            startDate: new Date("2020-12-06"),
-            endDate: new Date("2020-12-06"),
-            summary: 'DIA DE LA CONSTITUCION DE ESPAÑITA'
-        }
-        ]});
+$(document).ready(function() {
+  $("#calendario").simpleCalendar({
+    months: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'November', 'Diciembre'],
+    days: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+    displayYear: true, // Display year in header
+    fixedStartDay: true, // Week begin always by monday or by day set by number 0 = sunday, 7 = saturday, false = month always begin by first day of the month
+    displayEvent: true, // Display existing event
+    disableEventDetails: false, // disable showing event details
+    disableEmptyDetails: true, // disable showing empty date details
+    events: [{
+      startDate: new Date("2020-12-05 21:35"),
+      endDate: new Date("2020-12-05"),
+      summary: 'CUMPLEAÑOS DE LA FANTÁSTICA Y MARAVILLOSA NURIA GARCÍA ROZAS'
+    }, {
+      startDate: new Date("2020-12-06"),
+      endDate: new Date("2020-12-06"),
+      summary: 'DIA DE LA CONSTITUCION DE ESPAÑITA'
+    }]
+  });
 });
 
 function start() {
@@ -64,22 +64,22 @@ function desplegar_contenido(x) {
   $(contenido).slideToggle();
 }
 
-function desplegar_asignatura(x){
-    var asignatura = document.getElementById(x);
-    asignatura.style.display ="block";
-    var home = document.getElementById("div_content");
-    home.style.display ="none";
-    $(".barralateralderecha").show();
+function desplegar_asignatura(x) {
+  var asignatura = document.getElementById(x);
+  asignatura.style.display = "block";
+  var home = document.getElementById("div_content");
+  home.style.display = "none";
+  $(".barralateralderecha").show();
 }
- 
-function desplegar_contenido_barra(x){
 
-    $(".div_content").hide();
-    $("#IU").hide();
-    $("#foroasignatura").hide();
-    $("#calificaciones").hide();
-    var contenido = document.getElementById(x);
-    contenido.style.display = "block";
+function desplegar_contenido_barra(x) {
+
+  $(".div_content").hide();
+  $("#IU").hide();
+  $("#foroasignatura").hide();
+  $("#calificaciones").hide();
+  var contenido = document.getElementById(x);
+  contenido.style.display = "block";
 
 }
 
@@ -95,47 +95,43 @@ function obtenerCookie(email) {
 }
 
 function inicioSesion() {
-    var emailInicio = document.getElementById('inicioemail').value;
-    var contraseñaInicio = document.getElementById('iniciocontraseña').value;
-    var elementosCookie = obtenerCookie(emailInicio);
-    var contraseñaCookie = elementosCookie[0];
-    var rolCookie = elementosCookie[3];
-    var usernameCookie = elementosCookie[1] + " " + elementosCookie[2];
-    if (contraseñaInicio === contraseñaCookie) {
-        cuentaActiva = emailInicio;
-        //window.addEventListener("resize", responsive);
-        document.getElementById('formularios').style.display="none";
-        document.getElementById('cabecera2').style.display="block";
-        document.getElementById('principal').style.display="flex";
-        document.getElementById('perfil').style.display="inline";
-        document.getElementById('abajo').style.display="block";
-        document.getElementById('lateral').style.display="block";
-        
-        if (rolCookie === "Estudiante") {
-            estudiante();
-        } else if(rolCookie === "Profesor"){
-            profesor();
-        }else{
-            administrador();
-        }
-        document.getElementById('nombreusuario').innerHTML=usernameCookie;
-        /*responsive();
-        Inicio();*/
+  var emailInicio = document.getElementById('inicioemail').value;
+  var contraseñaInicio = document.getElementById('iniciocontraseña').value;
+  var elementosCookie = obtenerCookie(emailInicio);
+  var contraseñaCookie = elementosCookie[0];
+  var rolCookie = elementosCookie[3];
+  var usernameCookie = elementosCookie[1] + " " + elementosCookie[2];
+  if (contraseñaInicio === contraseñaCookie) {
+    cuentaActiva = emailInicio;
+    //window.addEventListener("resize", responsive);
+    document.getElementById('formularios').style.display = "none";
+    document.getElementById('cabecera2').style.display = "block";
+    document.getElementById('principal').style.display = "flex";
+    document.getElementById('perfil').style.display = "inline";
+    document.getElementById('abajo').style.display = "block";
+    document.getElementById('lateral').style.display = "block";
+
+    if (rolCookie === "Estudiante") {
+      estudiante();
+    } else if (rolCookie === "Profesor") {
+      profesor();
+    } else {
+      administrador();
     }
     document.getElementById('nombreusuario').innerHTML = usernameCookie;
     /*responsive();
     Inicio();*/
-  
+  }
 }
 
-function estudiante(){
-    $(".admin").hide();
-    $(".profesor").hide();
-    $(".estudiante").show();
-    crear_notas();
-    $("#TABLA_ESTUDIANTE").show();
+function estudiante() {
+  $(".admin").hide();
+  $(".profesor").hide();
+  $(".estudiante").show();
+  crear_notas();
+  $("#TABLA_ESTUDIANTE").show();
 
-   
+
 }
 
 //MOSTRAR ROL PROFESOR
@@ -163,41 +159,41 @@ function cerrarSesion() {
   }
 }
 
-function Inicio(){
-    $("#div_content").show();
-    $("#IU").hide();
-    $("#calificaciones").hide();
-    $(".barralateralderecha").hide();
+function Inicio() {
+  $("#div_content").show();
+  $("#IU").hide();
+  $("#calificaciones").hide();
+  $(".barralateralderecha").hide();
 
 }
 //ABRIR Y CERRAR UN HILO DEL FORO
-function alternarForo(x){
-    var mensajesForo=document.getElementById('hilo'+ x);
-    $(mensajesForo).slideToggle();
+function alternarForo(x) {
+  var mensajesForo = document.getElementById('hilo' + x);
+  $(mensajesForo).slideToggle();
 }
 
 //ENVIAR MENSAJE POR EL FORO
-function enviarMensajeForo(i){
-    var mensaje=document.getElementById("usermensaje"+i).value;
-    var cookieActual=obtenerCookie(cuentaActiva);
-    var nombre=cookieActual[1]+' '+cookieActual[2];
-    var hoy= new Date();
-    var fecha =hoy.getHours() + ":" + hoy.getMinutes() + " "+ hoy.getDate() + "/" + (hoy.getMonth()+1) + "/" + hoy.getFullYear();
-    $("#mensajesforo"+i).append('<div class="mensajeusuarioenvio"> <div class="informacionusuario"> <div class="fechamensajeenvio">'+
-    '<p>'+fecha+'</p></div>'+
-    '<div class="nombreestudianteenvio"><p>'+nombre+'</p> </div>'+
-    '<div class="estudianteconversacion">'+
-    '<img src="./images/usericon.png" id="usericon" alt="usericon"></div>'+
-    '</div><div class="mensajeforo"><p>'
-    +mensaje+'</p></div></div>');
-    var numReplicas=Number(document.getElementById("replicas"+i).textContent);
-    document.getElementById("replicas"+i).innerHTML=numReplicas+1;
-    document.getElementById('ultimomensaje'+i).innerHTML=fecha;
-    document.getElementById("usermensaje"+i).value="";
+function enviarMensajeForo(i) {
+  var mensaje = document.getElementById("usermensaje" + i).value;
+  var cookieActual = obtenerCookie(cuentaActiva);
+  var nombre = cookieActual[1] + ' ' + cookieActual[2];
+  var hoy = new Date();
+  var fecha = hoy.getHours() + ":" + hoy.getMinutes() + " " + hoy.getDate() + "/" + (hoy.getMonth() + 1) + "/" + hoy.getFullYear();
+  $("#mensajesforo" + i).append('<div class="mensajeusuarioenvio"> <div class="informacionusuario"> <div class="fechamensajeenvio">' +
+    '<p>' + fecha + '</p></div>' +
+    '<div class="nombreestudianteenvio"><p>' + nombre + '</p> </div>' +
+    '<div class="estudianteconversacion">' +
+    '<img src="./images/usericon.png" id="usericon" alt="usericon"></div>' +
+    '</div><div class="mensajeforo"><p>' +
+    mensaje + '</p></div></div>');
+  var numReplicas = Number(document.getElementById("replicas" + i).textContent);
+  document.getElementById("replicas" + i).innerHTML = numReplicas + 1;
+  document.getElementById('ultimomensaje' + i).innerHTML = fecha;
+  document.getElementById("usermensaje" + i).value = "";
 }
 
 //Métodos para descargar la tabla de calificaciones en formato excel.
-$(".export_e").click(function () {
+$(".export_e").click(function() {
   $("#TABLA_ESTUDIANTE").table2excel({
     // exclude CSS class
     exclude: ".noExl",
@@ -206,7 +202,7 @@ $(".export_e").click(function () {
   });
 });
 
-$(".export_p").click(function () {
+$(".export_p").click(function() {
   $("#TABLA_PROFESOR").table2excel({
     // exclude CSS class
     exclude: ".noExl",
